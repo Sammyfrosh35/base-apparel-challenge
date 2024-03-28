14dc00716bb0c233a5af76029fc0264a6568eef0
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-03-28 02:08:27 +0100

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "../src/assets/components/Hero/Hero";
+import './App.css'
+
+function App() {
+  return (
+   <div className="App">
+ <Hero />
+   </div>
+  )
+}
+
+export default App
